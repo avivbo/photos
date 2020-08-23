efe08dc76a6c097373c2e7d29359a7f4d22596c2
Aviv Boaz <avivbo287@gmail.com>
2020-08-23 18:04:37 +0300

    creation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      items_to_show: [],
+      item_index: 0,
+    };
+    this.pickRandomItem = this.pickRandomItem.bind(this);
+    this.pickMultipleItems = this.pickMultipleItems.bind(this);
+    this.setItemIndex = this.setItemIndex.bind(this);
+    this.loadData = this.loadData.bind(this);
+  }
+
+  loadData() {
+    fetch("http://j0.wlmediahub.com/App_Themes/api/test/photos.js")
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        this.setState({ items: data.photo });
+      })
+      .then(() => {
+        this.pickMultipleItems();
+      });
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  pickRandomItem() {
+    const index = Math.floor(Math.random() * this.state.items.length);
+    const item = {
+      ...this.state.items[index],
+    };
+    this.setState({
+      items: this.state.items.filter((itemA) => itemA.id !== item.id),
+    });
+    return item;
+  }
+
+  pickMultipleItems() {
+    const items = [];
+    for (let i = 0; i < 5; i++) {
+      items.push(this.pickRandomItem());
+    }
+
+    this.setState({
+      items_to_show: items,
+    });
+  }
+
+  setItemIndex(e) {
+    this.setState({
+      item_index: e.target.value,
+    });
+  }
+
+  render() {
+    const item = this.state.items_to_show[this.state.item_index]
+      ? this.state.items_to_show[this.state.item_index]
+      : {};
+    return (
+      <div className="App">
+        <div className="item">
+          <img src={item.img} />
+          <div className="title">{item.title}</div>
+          <div className="description">
+            {item.description || "There is no description"}
+          </div>
+        </div>
+
+        <div className="flex-nav">
+          <div className={`index-${this.state.item_index}`}>
+            <button onClick={this.setItemIndex} value={0}>
+              1
+            </button>
+            <button onClick={this.setItemIndex} value={1}>
+              2
+            </button>
+            <button onClick={this.setItemIndex} value={2}>
+              3
+            </button>
+            <button onClick={this.setItemIndex} value={3}>
+              4
+            </button>
+            <button onClick={this.setItemIndex} value={4}>
+              5
+            </button>
+          </div>
+          <button onClick={this.pickMultipleItems}>shuffle</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
